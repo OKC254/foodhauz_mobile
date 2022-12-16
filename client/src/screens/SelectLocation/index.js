79633cc1356714d17a85fd4132a75dcfe9b41102
Sau Juliana <julienyange@gmail.com>
2022-12-16 12:29:18 +0300

    feat: integrated maps and location selection and search

--- client/src/screens/SelectLocation/index.js
+++ client/src/screens/SelectLocation/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import React, { useState, useRef,useEffect } from 'react'
 import { Platform, StyleSheet, Dimensions, TextInput } from 'react-native'
+import {GOOGLE_MAPS_APIKEY} from "@env";
 import {
   Box,
   Button,
@@ -18,6 +19,7 @@ import {
   Input,
   Pressable,
   useToast,
+  KeyboardAvoidingView,
 } from "native-base";
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { TouchableOpacity } from 'react-native-gesture-handler'
@@ -25,9 +27,12 @@ import { colors } from '../../theme'
 import MapDisplay from '../../components/Map'
 import RequestReceivedAlert from '../../components/RequestReceivedAlert'
 import { useAuth } from '../../hooks/useAuth'
-import { DonationPackState } from '../../context'
+import { DonationPackState} from '../../context'
 import { BASE_API_URL } from '../../utils/api'
 import axios from 'axios';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { MaterialIcons } from '@expo/vector-icons';
+import { clearPack } from '../../utils/pack.utils';
 
 const screenHeight = Dimensions.get('window').height
 const screenWidth = Dimensions.get('window').width
@@ -61,15 +66,20 @@ const styles = StyleSheet.create({
 
 const SelectLocation = ({ navigation }) => {
   const [show, setShow] = React.useState(false)
+  const [origin, setOrigin] = useState({
+    location: "",
+    description: "",
+    address: "",
+  });
     const [error, setError] = React.useState("");
     const auth = useAuth();
-    const {donationPack} = DonationPackState()
+    const {donationPack, setDonationPack} = DonationPackState()
     const [loading, setLoading] = useState(false);
     const toast = useToast();
     const toastRef = useRef();
   const data = {
     "foods": donationPack,
-    "location": ["50.5", "70.0"],
+    "location": origin,
     "creator": auth.user._id,
     "cancelled": false,
     "approved": false,
@@ -95,6 +105,7 @@ const SelectLocation = ({ navigation }) => {
 
          // Success 🎉
          console.log("response", response);
+         clearPack(setDonationPack)
          setShow(true)
        }
       console.log(data)
@@ -126,83 +137,123 @@ const SelectLocation = ({ navigation }) => {
   };
   return (
     <SafeAreaView style={styles.root}>
-      <StatusBar barStyle="light-content" />
-      <VStack h="100%" w="100%" >
-        <View>
-          <Box bg="transparent" position={"absolute"} top={2} zIndex="10">
-            <Box
-              alignItems="center"
-              h="70px"
-              w={screenWidth}
-              position="relative"
-              onPress={() => {
-                navigation.navigate("NewDonationPack");
-              }}
-            >
-              <HStack paddingTop="20px" alignItems="center">
-                <Pressable
-                  onPress={() => {
-                    navigation.navigate("DonationPackStart");
-                  }}
-                >
-                  <Box ml="20px" p="10px" bg="white" rounded="md">
-                    <ChevronLeftIcon color="black" />
+      <KeyboardAvoidingView>
+        <StatusBar barStyle="light-content" />
+        <VStack h="100%" w="100%">
+          <View>
+            <Box bg="transparent" position={"absolute"} top={2} zIndex="10">
+              <Box
+                alignItems="center"
+                h="70px"
+                w={screenWidth}
+                position="relative"
+                onPress={() => {
+                  navigation.navigate("NewDonationPack");
+                }}
+              >
+                <HStack paddingTop="20px" alignItems="center">
+                  <Pressable
+                    onPress={() => {
+                      navigation.navigate("DonationPackStart");
+                    }}
+                  >
+                    <Box ml="20px" p="10px" bg="white" rounded="md">
+                      <ChevronLeftIcon color="black" />
+                    </Box>
+                  </Pressable>
+                  <Spacer />
+
+                  <Spacer />
+                  <Box mr="20px" p="10px" bg="white" rounded="md">
+                    <ThreeDotsIcon color="black" />
                   </Box>
-                </Pressable>
-                <Spacer />
-                <Text color="transparent" fontSize="20px" fontWeight="700">
-                  .
-                </Text>
-                <Spacer />
-                <Box mr="20px" p="10px" bg="white" rounded="md">
-                  <ThreeDotsIcon color="black" />
-                </Box>
-              </HStack>
-            </Box>
-          </Box>
-          <View h={500}>
-            <MapDisplay />
-          </View>
-          <VStack
-            pt="10"
-            alignItems="center"
-            justifyContent="center"
-            backgroundColor="white"
-          >
-            <Text fontWeight="bold" fontSize="20px" te>
-              Select pick-up Location
-            </Text>
-            <Box alignItems="center">
-              <Box mb="5">
-                <Input
-                  height="50"
-                  width="290"
-                  mt="5"
-                  variant="rounded"
-                  placeholder="Search Location...."
-                  InputRightElement={<Icon/>}
-                />
+                </HStack>
               </Box>
             </Box>
-            <Button
-              borderRadius="50px"
-              h="50px"
-              w="80%"
-              textAlign="center"
-              bg={colors.primary_color}
-              position="relative"
-              onPress={createDonation}
+            <Box
+              w={screenWidth}
+              px={30}
+              py={5}
+              position={"absolute"}
+              top={16}
+              zIndex="10"
             >
-              Confirm Location
-            </Button>
-            <RequestReceivedAlert
-              show={show}
-              setShow={setShow}
-              navigation={navigation}
-            />
-          </VStack>
-        </View>
-      </VStack>
+              <GooglePlacesAutocomplete
+                placeholder="Search Location"
+                nearbyPlacesAPI="GooglePlacesSearch"
+                enablePoweredByContainer={false}
+                fetchDetails={true}
+                debounce={400}
+                minLength={2}
+                onPress={(data, details = null) => {
+                  console.log(details);
+                  setOrigin({
+                    location: details.geometry.location,
+                    description: details.description,
+                    address: details.formatted_address,
+                  });
+                }}
+                query={{
+                  key: GOOGLE_MAPS_APIKEY,
+                  language: "en",
+                }}
+                styles={{
+                  container: {
+                    flex: 0,
+                  },
+                  textInput: {
+                    fontSize: 12,
+                    width: 200,
+                    backgroundColor: "#fafafa",
+                  },
+                }}
+              />
+            </Box>
+            <View h={500}>
+              <MapDisplay origin={origin} />
+            </View>
+            <VStack
+              pt="10"
+              alignItems="center"
+              justifyContent="center"
+              backgroundColor="white"
+            >
+              <Text fontWeight="bold" fontSize="20px" te>
+                Select pick-up Location
+              </Text>
+              <Box w={screenWidth} px={30} py={1}>
+                <Box mb="5">
+                  <Input
+                    height="50"
+                    width="290"
+                    mt="5"
+                    variant="rounded"
+                    placeholder="Search Location...."
+                    value={origin.address}
+                    InputRightElement={<MaterialIcons mr={5} size={24} name="location-pin" />}
+                  />
+                </Box>
+              </Box>
+              <Button
+                borderRadius="50px"
+                h="50px"
+                w="80%"
+                textAlign="center"
+                bg={colors.primary_color}
+                position="relative"
+                onPress={createDonation}
+              >
+                Confirm Location
+              </Button>
+              <RequestReceivedAlert
+                show={show}
+                setShow={setShow}
+                navigation={navigation}
+              />
+            </VStack>
+          </View>
+        </VStack>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
